928fb5e460b63b8cf42b18087072462b23558403
zero950 <zero950@hotmail.fr>
2015-11-04 14:54:48 +0100

    commit playlist

---
+++ src/MMITVBundle/Controller/PlaylistsController.php
@@ -0,0 +1,15 @@
+<?php
+namespace MMITVBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PlaylistsController extends Controller
+{
+    public function indexAction()
+    {
+     $em = $this->getDoctrine()->getManager();
+        $playlist= $em->getRepository('MMITVBundle:Video')->findAll();
+
+        return $this->render('MMITVBundle:Default:playlist.html.twig', array('playlist' => $playlist));
+    }
+}
